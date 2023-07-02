1ac5c4ba72c9d1b39e6c1c3f20cfc4764ceb71d1
Fascinated <admin@fascinated.cc>
2023-07-02 20:20:33 +0100

    add embed color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import Head from "next/head";
 import Config from "../../config.json";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,6 +15,9 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
+			<Head>
+				<meta name="theme-color" content={Config.metadata.color} />
+			</Head>
 			<body className={inter.className}>{children}</body>
 		</html>
 	);
