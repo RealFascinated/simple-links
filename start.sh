f6e4e98965351a41aa85affb1640e2774b0d979b
Fascinated <admin@fascinated.cc>
2023-07-12 14:16:41 +0100

    add basic download test

--- start.sh
+++ start.sh
@@ -1,6 +1,25 @@
+workDir=/usr/src/app
+
+baseUrl=https://git.fascinated.cc/Fascinated/simple-links/raw/branch/master/public/
+toDownload=("avatar.webp" "background.jpg")
+
 echo "Checking if files need to be downloaded"
-if [ -z "$(ls -A /usr/src/app/public)" ]; then
-    echo "Directory is empty"
+if [ -z "$(ls -A $workDir/public)" ]; then
+  echo "Some files are missing, downloading them"
+  if [ ! -w "$workDir" ]; then
+    echo "The directory \"$workDir\" is not writeable, unable to download files. Please check your docker compose for :ro and set it to :rw"
+  for item in "${toDownload[@]}"
+  do
+    dir=$workDir/public/$item
+    if [ -e "$dir" ]; then
+      echo "Downloading $item to directory $dir"
+      wget -P $dir $baseUrl$item
+      echo "Done"
+    fi
+  done
+  echo "Finished downloading files!"
+else
+  echo "All files are already present, no need to download"
 fi
 
 echo "Starting Simple Links..."
